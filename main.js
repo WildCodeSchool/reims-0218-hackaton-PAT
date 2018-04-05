8868a35a8ac40f804e9ab339dbfbce300858dffc
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-04-05 21:22:14 +0200

    factorisation du fetch dans la fonction fillHero

--- main.js
+++ main.js
@@ -1,41 +1,42 @@
-const getImage = (idHero, taille, position) => {
-    const heroes = fetch ("https://akabab.github.io/superhero-api/api/all.json")
-    .then(res => res.json())
-    .then(heroes => {
-        const image = heroes[idHero].images[taille]
+const getImage = (objetHeros, idHero, taille, position) => {
+        const image = objetHeros[idHero].images[taille]
         const imgSource = document.getElementById(`img-perso${position}`)
-        imgSource.setAttribute("src",image)
-    })
+   /*     if (imgsource)  // si j'ai une balise img avec le bon id dans le html
+            imgSource.setAttribute("src",image)
+        else
+            imgSource.setAttribute("src",`http://via.placeholder.com/350x150/?text='sorry, no images'`)*/
 }
 
-const getName = (idHero, position) => {
+const getName = (objetHero, idHero, position) => {
+    const name = objetHero[idHero].name
+    const nameSource = document.getElementById(`name-perso${position}`)
+    nameSource.innerHTML=name
+}
+
+
+const getStat = (objetHero, idHero, position) => {
+    const stat = objetHero[idHero].powerstats
+    const combatSource = document.getElementById(`combat${position}`)
+    combatSource.innerHTML=`combat: ${stat["combat"]}`
+    const powerSource = document.getElementById(`power${position}`)
+    powerSource.innerHTML=`power: ${stat["power"]}`
+    const intelSource = document.getElementById(`intelligence${position}`)
+    intelSource.innerHTML=`intelligence: ${stat["intelligence"]}`
+}
+
+const fillHero = (idHero, taille, position) => {
     const heroes = fetch ("https://akabab.github.io/superhero-api/api/all.json")
     .then(res => res.json())
     .then(heroes => {
-        const name = heroes[idHero].name
-        const nameSource = document.getElementById(`name-perso${position}`)
-        nameSource.innerHTML=name
+        getImage(heroes,idHero,taille,position)
+        getName(heroes,idHero, position)
+        getStat(heroes,idHero, position)
     })
 }
 
-const getStat = (idHero, position) => {
-    const heroes = fetch ("https://akabab.github.io/superhero-api/api/all.json")
-    .then(res => res.json())
-    .then(heroes => {
-        const stat = heroes[idHero].powerstats
-        const combatSource = document.getElementById(`combat${position}`)
-        combatSource.innerHTML=`combat: ${stat["combat"]}`
-        const powerSource = document.getElementById(`power${position}`)
-        powerSource.innerHTML=`power: ${stat["power"]}`
-        const intelSource = document.getElementById(`intelligence${position}`)
-        intelSource.innerHTML=`intelligence: ${stat["intelligence"]}`
-    })
-}
 
-getImage(122,"sm",2)
-getName(122,2)
-getStat(122,2)
-getImage(562, "sm", 4)
-getName(562,4)
-getStat(562,4)
+for (let i=0;i<10; i++) {
+    const idAleatoire = Math.floor(Math.random()*562)
+    fillHero(idAleatoire,"sm", i)
+}
 
