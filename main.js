53f55dac4985dbf0760210912e24cbbce101e7c5
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-04-05 18:35:54 +0200

    function getName added

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-const getImage = (idHero, taille) => {
+const getImage = (idHero, taille, position) => {
     const heroes = fetch ("https://akabab.github.io/superhero-api/api/all.json")
     .then(res => res.json())
     .then(heroes => {
         const image = heroes[idHero].images[taille]
-        console.log(image)
+        const imgSource = document.getElementById(`img-perso${position}`)
+        imgSource.setAttribute("src",image)
     })
 }
 
-getImage(2,"xs")
+const getName = (idHero, position) => {
+    const heroes = fetch ("https://akabab.github.io/superhero-api/api/all.json")
+    .then(res => res.json())
+    .then(heroes => {
+        const name = heroes[idHero].name
+        const nameSource = document.getElementById(`name-perso${position}`)
+        nameSource.innerHTML=name
+    })
+}
+
+getImage(127,"md",2)
+getName(127,2)
 
