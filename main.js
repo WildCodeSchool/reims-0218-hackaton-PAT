704d9568ee4636838ab077596693b2f657ff1ada
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-04-05 19:33:09 +0200

    functions getImage, getName, getStats

--- main.js
+++ main.js
@@ -18,6 +18,24 @@ const getName = (idHero, position) => {
     })
 }
 
-getImage(127,"md",2)
-getName(127,2)
+const getStat = (idHero, position) => {
+    const heroes = fetch ("https://akabab.github.io/superhero-api/api/all.json")
+    .then(res => res.json())
+    .then(heroes => {
+        const stat = heroes[idHero].powerstats
+        const combatSource = document.getElementById(`combat${position}`)
+        combatSource.innerHTML=`combat: ${stat["combat"]}`
+        const powerSource = document.getElementById(`power${position}`)
+        powerSource.innerHTML=`power: ${stat["power"]}`
+        const intelSource = document.getElementById(`intelligence${position}`)
+        intelSource.innerHTML=`intelligence: ${stat["intelligence"]}`
+    })
+}
+
+getImage(122,"sm",2)
+getName(122,2)
+getStat(122,2)
+getImage(562, "sm", 4)
+getName(562,4)
+getStat(562,4)
 
